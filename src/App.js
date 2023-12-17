23c22fe8f5278f0831f81cfc239ba0664f67c34a
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-17 22:26:14 +0100

    feat: Import home into root app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       <div className="content">
       </div>
     </div>
