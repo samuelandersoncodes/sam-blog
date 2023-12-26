c40c06e85cf7b12b25b2649c58aef02bd0bd11e7
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-26 22:25:04 +0100

    feat: Add delete button to bloglist

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,12 @@
-const BlogList = ({blogs}) => {
-  
+const BlogList = ({ blogs, handleDelete }) => {
+
   return (
     <div className="blog-list">
-      {blogs.map(({title, id, author,}) => (
+      {blogs.map(({ title, id, author, }) => (
         <div className="blog-preview" key={id}>
           <h2>{title}</h2>
           <p>Written by {author}</p>
+          <button onClick={() => handleDelete(id)}>Delete Blog</button>
         </div>
       ))}
     </div>
