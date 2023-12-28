7430a3edb7b4cdd757593c79fbf5ea4ff3caf474
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-28 22:07:22 +0100

    feat: Set blogs state to null

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My Django Journey', body: 'I have built 2 projects', author: 'Sam', id: 1 },
-    { title: 'My React Journey', body: 'I just started', author: 'Sam', id: 2 },
-    { title: 'Unittest', body: 'I always use it', author: 'Sam', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
