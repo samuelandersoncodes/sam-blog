ae2d0c3db00549783d026bb37c38692b6f92f54d
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-28 22:29:19 +0100

    refactor: Remove handle delete functionality

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,6 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
@@ -22,7 +17,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
