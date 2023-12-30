4e22e4f99cab70dd0edadd74c932ac923b8ad00f
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-30 21:21:51 +0100

    refactor: Remove fetch functionlity from home

--- src/Home.js
+++ src/Home.js
@@ -7,27 +7,6 @@ const Home = () => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-        .then(res => {
-          if (!res.ok) {
-            throw Error('Could not fetch data for that resource');
-          }
-          return res.json();
-        })
-        .then(data => {
-          setBlogs(data)
-          setIsPending(false);
-          setError(null);
-        })
-        .catch(err => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 600);
-  }, []);
-
   return (
     <div className="home">
       {error && <div>{error}</div>}
