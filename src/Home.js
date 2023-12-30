70c18a21cd641e01e362526c10b0c8db750df68b
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-30 20:35:58 +0100

    feat: Set error to null in fetch data block

--- src/Home.js
+++ src/Home.js
@@ -19,6 +19,7 @@ const Home = () => {
         .then(data => {
           setBlogs(data)
           setIsPending(false);
+          setError(null);
         })
         .catch(err => {
           setIsPending(false);
