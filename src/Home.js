992cc1a581dc63edf008a68533dd20255fb8e16e
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-30 21:49:45 +0100

    feat: Implement usefetch functionality in home

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');  
 
   return (
     <div className="home">
