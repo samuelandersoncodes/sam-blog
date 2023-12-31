6aba116d7c2e38a456bf148b85fe5975857d31b1
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-31 20:38:47 +0100

    feat: Return the create heading

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,9 @@
-const  = () => {
-  return (  );
+const Create = () => {
+  return ( 
+    <div className="create">
+      <h2>Add a New Blog</h2>
+    </div>
+   );
 }
  
-export default ;
+export default Create;
