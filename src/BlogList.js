f93eeda43d6a8262b9ab1adb02cf68e276f4523d
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-31 21:56:42 +0100

    feat: Make blog titles links

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({ blogs }) => {
 
   return (
     <div className="blog-list">
       {blogs.map(({ title, id, author, }) => (
         <div className="blog-preview" key={id}>
-          <h2>{title}</h2>
-          <p>Written by {author}</p>
+          <Link to={`/blogs/${id}`}>
+            <h2>{title}</h2>
+            <p>Written by {author}</p>
+          </Link>
         </div>
       ))}
     </div>
