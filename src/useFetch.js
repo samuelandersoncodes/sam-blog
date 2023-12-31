8018aed4f39f713a9ffb9cc2ecf4f642499fed47
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2023-12-31 21:11:48 +0100

    feat: Clean up data fetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,8 +7,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then(res => {
           if (!res.ok) {
             throw Error('Could not fetch data for that resource');
@@ -21,10 +23,15 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch(err => {
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
           setIsPending(false);
           setError(err.message);
+          }
         });
     }, 600);
+    return () => abortCont.abort();
   }, [url]);
   return { data, isPending, error };
 };
