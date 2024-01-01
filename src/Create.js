bfaddab4ead3884476381f1c9468db0f9ccab0b7
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-01-01 21:23:12 +0100

    feat: Add new blog form title

--- src/Create.js
+++ src/Create.js
@@ -3,7 +3,11 @@ const Create = () => {
     <div className="create">
       <h2>Add a New Blog</h2>
       <form>
-        
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+        />
       </form>
     </div>
    );
