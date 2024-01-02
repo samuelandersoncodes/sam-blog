33cebfea5799257f347136cea541fbd5bb29c2c9
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-01-02 18:32:16 +0100

    feat: Set ispending in the handlesubmit function

--- src/Create.js
+++ src/Create.js
@@ -8,10 +8,13 @@ const Create = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
+    setIsPending(true);
     fetch('http://localhost:8000/blogs', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
+    }).then(() => {
+      setIsPending(false);
     });
   }
 
