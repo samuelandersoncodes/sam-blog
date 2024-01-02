7eb24bac6a9b4faaf19cc2d24c8a7c987d52fee1
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-01-02 19:16:46 +0100

    feat: Add delete functionality to blog detail

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -4,6 +4,11 @@ import useFetch from "./useFetch";
 const BlogDetail = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    })
+  };
 
   return (
     <div className="blog-detail">
