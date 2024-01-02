64e711968476aadf7e0bdf9d5b31c54a70790c3c
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-01-02 22:26:53 +0100

    feat: Include notfound in app js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetail from './BlogDetail';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path="/blogs/:id">
             <BlogDetail/>
+          </Route>
+          <Route path="*">
+            <NotFound/> 
           </Route>
         </Switch>
         <div className="content">
